35d4302bf1153b09a3b2a51678d1db91f19839c9
Hyeokkyun Jo <whgurrbs@gmail.com>
2022-06-18 00:26:55 +0900

    feat: 5_5 props, add comment element

---
+++ src/chapter_05/CommentList.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { createPortal } from 'react-dom';
+import Comment from './Comment';
+
+const comments = [
+  {
+    name: '조혁균',
+    comment: '1번째 댓글일지도?'
+  },
+  {
+    name: '조혁균',
+    comment: '2번째 댓글일지도?'
+  },
+  {
+    name: '조혁균',
+    comment: '3번째 댓글일지도?'
+  }
+];
+
+function CommentList(props) {
+  return (
+    <div>
+      {comments.map((comment) => {
+        return <Comment name={comment.name} comment={comment.comment} />;
+      })}
+    </div>
+  );
+}
+
+export default CommentList;
