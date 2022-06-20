f786bdd93fad441562edd284277e74959aa3d838
Hyeokkyun Jo <whgurrbs@gmail.com>
2022-06-20 22:58:10 +0900

    feat: 7_4 hooks, add Accommodate element

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import Library from './chapter_03/Library';
 import Clock from './chapter_04/Clock';
 import CommentList from './chapter_05/CommentList';
 import NotificationList from './chapter_06/NotificationList';
+import Accomodate from './chapter_07/Accommodate';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
@@ -30,9 +31,15 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 //   </React.StrictMode>
 // );
 
+// root.render(
+//   <React.StrictMode>
+//     <NotificationList />
+//   </React.StrictMode>
+// );
+
 root.render(
   <React.StrictMode>
-    <NotificationList />
+    <Accomodate />
   </React.StrictMode>
 );
 
